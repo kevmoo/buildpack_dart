fc88a569ad0ad33b99339f0dd9021d73287cb4b5
Kevin Moore <kevmoo@google.com>
2022-05-11 20:17:10 -0700

    more simple

--- lib/buildpack.dart
+++ lib/buildpack.dart
@@ -67,11 +67,11 @@ Future<HttpServer> run() async {
   final ip = InternetAddress.anyIPv4;
 
   // Configure a pipeline that logs requests.
-  final _handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+  final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
 
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final server = await serve(_handler, ip, port);
+  final server = await serve(handler, ip, port);
   print('Server listening on port ${server.port}');
   return server;
 }
